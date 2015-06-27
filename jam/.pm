d7086357191df19a9f1acfb554af50c554074f25
Aditya Vohra <adityavohra7@MacBook-Pro.local>
2015-06-27 16:41:36 -0700

    Added some basic infrastructure files that Django created when I made the jam project. (I've also added a user_profile sub folder that would represent the userprofile page)

---
+++ jam/.pm
@@ -0,0 +1,20 @@
+package jam;
+
+# This module has been generated using Parse::Eyapp::Treereg
+# from file jam/. Don't modify it.
+# Change jam/ instead.
+# Copyright (c) Casiano Rodriguez-Leon 2006. Universidad de La Laguna.
+# You may use it and distribute it under the terms of either
+# the GNU General Public License or the Artistic License,
+# as specified in the Perl README file.
+
+use strict;
+use warnings;
+use Carp;
+use Parse::Eyapp::_TreeregexpSupport qw(until_first_match checknumchildren);
+
+our @all = ( ) = Parse::Eyapp::YATW->buildpatterns();
+
+
+1;
+
